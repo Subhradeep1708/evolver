4d3d3895e9603955c79851c2108e4626f1675a12
Raktim-Mitra <mitraraktim294@gmail.com>
2025-01-03 01:32:40 +0530

    feat: update Welcome component layout and design with new images and button

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,17 +1,25 @@
-import { Button, VStack } from "@chakra-ui/react";
-import { Toaster, toaster } from "./ui/toaster.jsx";
-// import { useRef, useState } from "react";
+import { Box, Button, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { FaArrowRight } from "react-icons/fa";
+
 
 function Welcome() {
-    const sayHello = () => {
-        toaster.create({ title: "Hello Raktim" });
-    };
-
+   
     return (
-        <VStack>
-            <Button onClick={sayHello}>Say Hello</Button>
-            <Toaster />
-        </VStack>
+      
+          <HStack h={"100vh"} w={"100vw"} justifyContent={"space-between"} >
+            <VStack display={"flex"} flexDirection={"column"} p={"20"}alignItems={"flex-start"} w={"60%"} position={"relative"} >
+                <Image src="../../../public/clouds.png" position={"absolute"} top={"-10"} left={"0"} zIndex={"10"} w={"300px"} h={"200px"}/>
+                <Box mb={"10"} zIndex={"11"}>
+                <Heading size={"6xl"} color={"#324667"} pb={"2.5"} >Evolver..</Heading>
+                <Text fontSize={"xl"}>Welcome to the best online exam portal, test yourself here</Text>
+                </Box>
+                <Button p={"5"} size="lg" bg={"#D6E6FE"} color={"#102353"}>Login to continue <FaArrowRight /></Button>
+            </VStack>
+            <Box w={"40%"} pr={"20"} >
+                <Image src="../../../public/image.png"/>
+            </Box>
+          </HStack>
+       
     );
 }
 export default Welcome;
