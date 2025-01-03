5d7ebbf502675364b940be1a079dd720a5713441
Sanchari Mandal <113378204+Hul0@users.noreply.github.com>
2025-01-03 13:28:13 +0530

    feat: add HomeNav component and integrate it into Welcome component layout

---
+++ frontend/src/components/HomeNav.jsx
@@ -0,0 +1,54 @@
+import { Box, HStack, Text } from "@chakra-ui/react";
+import { NavLink } from "react-router";
+
+const HomeNav = () => {
+    return (
+        <HStack
+            w={"full"}
+            h={"8vh"}
+            position={"absolute"}
+            maxW={"breakpoint-sm"}
+            background={"#D6E6FE"}
+            my={"8"}
+            rounded={"full"}
+            justify={"space-evenly"}
+        >
+            {links.map((link) => (
+                <Box key={link.name} basis={"1/4"}>
+                    <NavLink to={link.url} key={link.name} basis={"1/4"}>
+                        <Text
+                            px={"4"}
+                            py={"2"}
+                            color={"#102353"}
+                            fontWeight={"bold"}
+                            fontSize={"lg"}
+                            textAlign={"center"}
+                        >
+                            {link.name}
+                        </Text>
+                    </NavLink>
+                </Box>
+            ))}
+        </HStack>
+    );
+};
+export default HomeNav;
+
+const links = [
+    {
+        name: "Home",
+        url: "/",
+    },
+    {
+        name: "About",
+        url: "/about",
+    },
+    {
+        name: "Contact",
+        url: "/contact",
+    },
+    {
+        name: "FAQ",
+        url: "/faq",
+    },
+];
