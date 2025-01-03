dd49401adac7f3fb641c6dad113c937d507b604f
Raktim-Mitra <mitraraktim294@gmail.com>
2025-01-03 14:15:27 +0530

    feat: enhance auth state management by adding persistence with Zustand middleware

--- frontend/src/Store/auth_slice.js
+++ frontend/src/Store/auth_slice.js
@@ -1,4 +1,12 @@
-export const createAuthSlice=(set)=>({
-    user:undefined,
-    setUser:(user)=>set({user}),
-})
+import {persist} from 'zustand/middleware'
+export const createAuthSlice = (set) =>
+    persist(
+      (set) => ({
+        user: undefined,
+        setUser: (user) => set({ user }),
+      }),
+      {
+        name: "auth-storage", // Key for localStorage
+        getStorage: () => localStorage, // You can switch to sessionStorage if needed
+      }
+    );
