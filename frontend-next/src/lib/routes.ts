25ba4a291fcef2bbba2cc71ecc881f44b67a664d
Raktim-Mitra <mitraraktim294@gmail.com>
2025-05-20 14:09:49 +0530

    feat: reorganize API routes for better structure and clarity

--- frontend-next/src/lib/routes.ts
+++ frontend-next/src/lib/routes.ts
@@ -1,16 +1,39 @@
 export const origin = process.env.ORIGIN || "http://localhost:5000";
 const auth_routes = "api/auth";
-
+const user_routes = "api/user";
+const subject_routes = "api/subject";
+const exam_routes = "api/exam";
+const mcq_routes = "api/mcq";
+const answer_routes = "api/answer";
+const result_routes = "api/result";
 const apiRoutes = {
+    //*auth-routes
     studentLogin: `${origin}/${auth_routes}/student/login`,
-    logout: `${origin}/${auth_routes}/logout`,
     teacherLogin: `${origin}/${auth_routes}/teacher/login`,
     studentRegister: `${origin}/${auth_routes}/student/register`,
     teacherRegister: `${origin}/${auth_routes}/teacher/register`,
-    getAllSubject: `${origin}/api/subject`,
-    addSubject: `${origin}/api/subject`,
-    addExam: `${origin}/api/exam`,
-    addMcqBulk: `${origin}/api/mcq`,
+    logout: `${origin}/${auth_routes}/logout`,
+
+    //*user-routes
+    getStudent: `${origin}/${user_routes}/student`, // add student id to get specific student
+    getTeacher: `${origin}/${user_routes}/teacher`, // add teacher id to get specific teacher
+
+    //*subject-routes
+    getSubject: `${origin}/${subject_routes}`, // add subject id to get specific subject
+
+    //*exam-routes
+    getExam: `${origin}/${exam_routes}`, // add exam id to get specific exam
+    getExamByTeacher: `${origin}/${exam_routes}/teacher`, // add teacher id to get specific exam
+
+    //*mcq-routes
+    addMcq: `${origin}/${mcq_routes}`, 
+    
+    //*answer-routes
+    submitAnswer: `${origin}/${answer_routes}/submit`, // add exam id to get specific exam
+    
+    //*result-routes
+    getResult: `${origin}/${result_routes}`, // add exam id to get specific exam
+    getResultByStudent: `${origin}/${result_routes}/student`, // add student id to get specific exam
 };
 
 export default apiRoutes;
