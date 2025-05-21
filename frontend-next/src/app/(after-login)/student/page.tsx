e0aa2f431247b48954b6f79bb302f470d31fd2ba
Sanchari Mandal <113378204+Hul0@users.noreply.github.com>
2025-05-21 10:11:36 +0530

    feat: enhance Student and Teacher dashboard pages with exam fetching functionality

--- frontend-next/src/app/(after-login)/student/page.tsx
+++ frontend-next/src/app/(after-login)/student/page.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import apiRoutes from "@/lib/routes";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+type Exam = {
+    examName: string;
+    addedBy: string;
+    totalMarks: number;
+    description: string;
+    id: string | number;
+};
+import { useRouter } from "next/navigation";
 
 const StudentDashboardPage = () => {
-    return <div>StudentDashboardPage</div>;
+    const [exams, setExams] = useState<Exam[]>([]);
+    const router = useRouter();
+    const handleStartExam = (examId: any) => {
+        router.push(`/start/exam/${examId}`);
+    };
+
+    useEffect(() => {
+        const fetchExams = async () => {
+            const res = await axios.get(apiRoutes.getExam);
+            if (res.status === 200) {
+                // setExams(res.data.data);
+                console.log(res.data.exams);
+                setExams(res.data.exams);
+            }
+        };
+
+        fetchExams();
+    }, []);
+    console.log(exams);
+    return (
+        <div>
+            StudentDashboardPage
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {exams.map((exam, i) => {
+                    return (
+                        <div key={i}>
+                            <Card>
+                                <CardContent>
+                                    <CardTitle>{exam.examName}</CardTitle>
+                                    {exam.addedBy}
+                                    {exam.totalMarks} Marks
+                                    <CardDescription>
+                                        {exam.description}
+                                    </CardDescription>
+                                </CardContent>
+                                <CardFooter>
+                                    {/* <Text fontSize="sm" color="red">
+                                            Due at 10th October at 10:00 AM
+                                        </Text> */}
+                                    <Button
+                                        onClick={() => handleStartExam(exam.id)}
+                                    >
+                                        Start Exam
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default StudentDashboardPage;
--- frontend-next/src/app/(after-login)/teacher/page.tsx
+++ frontend-next/src/app/(after-login)/teacher/page.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useAppContext } from "@/context/AppContext";
+import apiRoutes from "@/lib/routes";
 
 const TeacherDashboardPage = () => {
+    const { user } = useAppContext();
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(
+                `${apiRoutes.getExamByTeacher}/${user?.userId}`,
+                {
+                    withCredentials: true,
+                }
+            );
+
+            setData(response.data);
+        };
+
+        fetchData();
+        console.log("data", data);
+    }, []);
+
     return <div>TeacherDashboardPage</div>;
 };
 
